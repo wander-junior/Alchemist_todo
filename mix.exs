68858b4762b8a37001ebed2b2a4d7dae180b0b9b
wander-junior <wanderjrcruz96@gmail.com>
2024-10-13 20:09:00 -0300

    chore: update elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule AlchemistTodo.MixProject do
     [
       app: :alchemist_todo,
       version: "0.1.0",
-      elixir: "~> 1.15-rc",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
